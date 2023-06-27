b902b2a95431c4b5fa9968ddaf1ee5beef9785f1
Jam19th <jam19th@gmail.com>
2023-06-26 21:00:54 -0700

    adding 404 page view

--- index.js
+++ index.js
@@ -13,10 +13,11 @@ app.get('/', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.render('error404')
 })
 
 app.listen(process.env.PORT, () => {
     console.log(`http://localhost:${process.env.PORT}`)
     console.log(`http://localhost:${process.env.PORT}/places`)
+    console.log(`http://localhost:${process.env.PORT}/404test`)
 })
