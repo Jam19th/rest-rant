f02a7a1db5a9b3632f9e40105b8388337e875a26
Jam19th <jam19th@gmail.com>
2023-06-29 11:37:07 -0700

    delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -44,6 +44,19 @@ router.post('/', (req, res) => {
     res.redirect('/places')
 })
 
-
+//Delete /places/:id
+router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        places.splice(id, 1)
+        res.redirect('/places')
+    }
+})
 
 module.exports = router;
